a1ba2c84d7d319f41bdb8bc8c987fe33df70beb0
aryehklempner <35413832+aryehklempner@users.noreply.github.com>
2018-12-12 22:37:38 -0800

    Add license comment header to server/main.c
    Also, MAX_POSS_ARGC is renamed MAX_POSS_VALID_ARGC.

--- server/main.c
+++ server/main.c
@@ -1,12 +1,22 @@
+/* (c) 2018 Aryeh Klempner <arayaydev@gmail.com>
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version. This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+more details. You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_POSS_ARGC 2
+#define MAX_POSS_VALID_ARGC 2
 
 int main(int argc, char *argv[]){
-	if(argc > 1) for(int i = 1; i < argc && i <= MAX_POSS_ARGC; i++){
+	if(argc > 1) for(int i = 1; i < argc && i <= MAX_POSS_VALID_ARGC; i++){
 		if(!strncmp((const char*)(argv[i]), "-h", 2) ||
 		!strncmp((const char*)(argv[i]), "--help", 6))
 			puts("Try \"info tesla\"");
