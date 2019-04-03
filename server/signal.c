3c459f0c94d9bea55c09fbd476973bb8679eff98
aryehklempner <35413832+aryehklempner@users.noreply.github.com>
2019-04-03 08:35:36 -0700

    Formatting change

--- server/signal.c
+++ server/signal.c
@@ -1,4 +1,4 @@
-/*(c) 2018-19 Aryeh Klempner <arayaydev@gmail.com>
+/*(c) 2019 Aryeh Klempner <arayaydev@gmail.com>
 This program is free software: you can redistribute it and/or modify it under
 the terms of the GNU General Public License (GPL) as published by the Free
 Software Foundation, either version 3 of the GPL, or (at your option) any
