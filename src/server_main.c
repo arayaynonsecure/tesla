73080a55c9750a58aead1fb87b5ce85f6fee5304
Aryeh Klempner <arayaydev@gmail.com>
2020-03-09 17:52:58 -0700

    Largely rewrote the existing codebase.

--- src/server_main.c
+++ src/server_main.c
@@ -1,4 +1,4 @@
-/*(c) 2018-19 Aryeh Klempner <arayaydev@gmail.com>
+/*(c) 2018-20 Aryeh Klempner <arayaydev@gmail.com>
 This program is free software: you can redistribute it and/or modify it under
 the terms of the GNU General Public License (GPL) as published by the Free
 Software Foundation, either version 3 of the GPL, or (at your option) any
@@ -14,62 +14,56 @@ have received a copy of the GPL with this program. If not, see
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_POSS_VALID_ARGC 4
+#define MAX_POSSIBLE_VALID_ARGC 4 //counting -h and -v
 
-uint8_t global_flags;
+//bits: 0=loud 1=quiet 2-8=rsvd
+uint8_t srv_flags;
 
-//void clean_exit(int sig);
 void register_handlers(void);
 
-static inline void helpchk(int argc, char* argv[]){
-	for(int i = 1; i < argc && i <= MAX_POSS_VALID_ARGC; i++){
+static inline void argchk(int argc, char* argv[]){
+	for(int i = 1; i < argc && i <= MAX_POSSIBLE_VALID_ARGC; i++){
 		if(!strncmp((const char*)(argv[i]), "-h", 2) ||
 		!strncmp((const char*)(argv[i]), "--help", 6)){
 			puts("Try \"info tesla\"");
 			exit(EXIT_SUCCESS);
 		}
-		else if(!strncmp((const char*)(argv[i]), "--version", 9)){
+		else if(!strncmp((const char*)(!strncmp((const char*)(argv[i]),
+		"--version", 9)){
 			puts("`tesla` is still in pre-alpha development");
 			exit(EXIT_SUCCESS);
 		}
+		else if(!strncmp((const char*)(argv[i]), "--loud", 6)){
+			srv_flags |= 0x01;
+		}
+		else if(!strncmp((const char*)(argv[i]), "--quiet", 7)){
+			srv_flags |= 0x02;
+		}
 	}
 }
 
-static inline uint8_t parse_args(int argc, char* argv[]){
-	register bool notify = true;
-	register uint8_t flags = 0;
-	for(int i = 1; i < argc && i <= MAX_POSS_VALID_ARGC; i++){
-		if(!strncmp((const char*)(argv[i]), "--config=/", 10)){
-			flags |= 0x01;
-			puts("Config file location overridden:");
-			puts((const char*)(argv[i] + 9));
+static inline void parse_args (int argc, char* argv[]){
+	for(int i = 1; i < argc && i <= MAX_POSSIBLE_VALID_ARGC; i++){
+		if(!strncmp((const char*)(argv[i]), "--loud", 6)){
+			continue;
 		}
-		else if(!strncmp((const char*)(argv[i]), "--dataset=/", 11)){
-			flags |= 0x02;
-			puts("Default dataset overridden:");
-			puts((const char*)(argv[i] + 10));
+		else if(!strncmp((const char*)(argv[i]), "--quiet", 7)){
+			continue;
 		}
-//For testing: loop infinitely. Uncomment to enable.
-/*		else if(!strncmp((const char*)(argv[i]), "--loop", 6)){
-			puts("NOTICE: entering infinite loop for testing");
-			while(true){
-				;
-			}
-		}
-*/
-		else if(notify){
-			puts("Unknown arg detected:");
+		else if(!(srv_flags & 0x02)){
+			printf("Unknown argument: ");
 			puts((const char*)(argv[i]));
-			puts("Is this a typo? See \"info tesla\" for help");
-			notify = false;
+			puts("Try `info tesla` for help");
 		}
 	}
-	return flags;
 }
 
 int main(int argc, char* argv[]){
-	helpchk(argc, argv);
+	if(argc > 1) argchk(argc, argv);
 	register_handlers();
+	if(argc > 1) parse_args(argc, argv);
+	exit(EXIT_SUCCESS);
+}
 	if(argc > 1) global_flags = parse_args(argc, argv);
 	else puts("No args given, trying default options");
 	exit(EXIT_SUCCESS);
